2fd47bdba4b9d6bfc26f51fb5b9a57a03dcf6893
Marjory PRIN <marjory.prin@gmail.com>
2023-10-09 10:43:51 +0200

    V6 : TP6

--- src/fr/diginamic/banque/TestBanque.java
+++ src/fr/diginamic/banque/TestBanque.java
@@ -5,7 +5,7 @@ import fr.diginamic.banque.entites.Compte;
 public class TestBanque {
 
 	public static void main(String[] args) {
-		Compte monCompte = new Compte(12345678, 1256.78);
+		Compte monCompte = new Compte("de1486ft45", 1200.00);
 		
 		System.out.println(monCompte);
 	}
--- src/fr/diginamic/banque/entites/Compte.java
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -1,13 +1,44 @@
 package fr.diginamic.banque.entites;
 
+/**
+ * Représente un compte bancaire
+ * @author Marjory PRIN
+ */
 public class Compte {
-	private int numeroCompte;
+	private String numeroCompte;
 	private double soldeCompte;
 	
-	public Compte(int numeroCompte, double soldeCompte) {
+	/**
+	 * Constructeur qui prends en paramètre le numéro de compte et le solde du compte
+	 * @param numeroCompte : numéro du compte bancaire
+	 * @param soldeCompte : solde du compte bancaire
+	 */
+	public Compte(String numeroCompte, double soldeCompte) {
 		this.numeroCompte = numeroCompte;
 		this.soldeCompte = soldeCompte;
 	}
+
+	@Override
+	public String toString() {
+		return "Compte [numeroCompte=" + numeroCompte + ", soldeCompte=" + soldeCompte + "]";
+	}
+
+	/**
+	 * @return the numeroCompte
+	 */
+	public String getNumeroCompte() {
+		return numeroCompte;
+	}
+
+	/**
+	 * @return the soldeCompte
+	 */
+	public double getSoldeCompte() {
+		return soldeCompte;
+	}
+
+	
+	
 	
 
 }
