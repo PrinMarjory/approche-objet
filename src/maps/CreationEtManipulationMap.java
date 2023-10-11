16c823fc3c7a0cf2bb7cd225c4d42c12e7ac609d
Marjory PRIN <marjory.prin@gmail.com>
2023-10-11 17:39:45 +0200

    V18 : TP 13 Maps

---
+++ src/maps/CreationEtManipulationMap.java
@@ -0,0 +1,43 @@
+package maps;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class CreationEtManipulationMap {
+
+	public static void main(String[] args) {
+		
+		//Initialisation
+		HashMap<Integer, String> mapVilles = new HashMap<>();
+		mapVilles.put(13, "Marseille");
+		mapVilles.put(34, "Montpellier");
+		mapVilles.put(44, "Nantes");
+		mapVilles.put(75, "Paris");
+		mapVilles.put(31, "Toulouse");
+		mapVilles.put(59, "Lille");
+		mapVilles.put(69, "Lyon");
+		mapVilles.put(33, "Bordeaux");
+		
+		//Affichage des clés contenues dans la map
+		System.out.println("Clés: ");
+		Iterator<Integer> keysIte = mapVilles.keySet().iterator();
+		while (keysIte.hasNext()) {
+			int key = keysIte.next();
+			System.out.print(key + " ");
+		}
+		System.out.println("\n-------------------------------------");
+		
+		//Affichage des valeurs contenues dans la map
+		System.out.println("Valeurs: ");
+		Iterator<String> valIte = mapVilles.values().iterator();
+		while (valIte.hasNext()) {
+			String value = valIte.next();
+			System.out.print(value + " ");
+		}
+		System.out.println("\n-------------------------------------");
+		
+		//Affichage de la taille de la map
+		System.out.println("Taille de la map : " + mapVilles.size());
+		
+	}
+
+}
---
+++ src/maps/MapVilles.java
@@ -0,0 +1,49 @@
+package maps;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import fr.diginamic.listes.Ville;
+
+
+public class MapVilles {
+
+	public static void main(String[] args) {
+		
+		// Instanciation Villes
+		Ville nice = new Ville("Nice", 343000);
+		Ville carcassone = new Ville("Carcassone", 47800);
+		Ville narbonne = new Ville("Narbonne", 53400);
+		Ville lyon = new Ville("Lyon", 484000);
+		Ville foix = new Ville("Foix", 9700);
+		Ville pau = new Ville("Pau", 77200);
+		Ville marseille = new Ville("Marseille", 850700);
+		Ville tarbes = new Ville("Tarbes", 40600);
+
+		// Création de la map
+		HashMap<String, Ville> map = new HashMap<String, Ville>();
+		map.put("Nice", nice);
+		map.put("Carcassone", carcassone);
+		map.put("Narbonne", narbonne);
+		map.put("Lyon", lyon);
+		map.put("Foix", foix);
+		map.put("Pau", pau);
+		map.put("Marseille", marseille);
+		map.put("Tarbes", tarbes);
+		
+		// Supression de la ville avec le moins d'habitant
+		Iterator<String> keyIte = map.keySet().iterator();
+		int minHab = Integer.MAX_VALUE;
+		String villeMin = "";
+		while (keyIte.hasNext()) {
+			String key = keyIte.next();
+			if (map.get(key).getNbHabitants() < minHab) {
+				minHab = map.get(key).getNbHabitants();
+				villeMin = key;
+			}
+		}
+		map.remove(villeMin);
+		System.out.println(map);
+		
+	}
+
+}
